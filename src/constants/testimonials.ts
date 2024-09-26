e187051f064e3c909c6927d930da6282adda7126
Felix-Asante <quamzgraphix@gmail.com>
2024-09-26 19:20:23 +0100

    testimonials updated

--- src/constants/testimonials.ts
+++ src/constants/testimonials.ts
@@ -1,8 +1,8 @@
 export const TESTIMONIALS = [
-  "/images/testimonials/Screenshot_20240704-234844[1] 1.png",
-  "/images/testimonials/Screenshot_20240705-133312[1] 1.png",
-  "/images/testimonials/Screenshot_20240705-134218[1] 1.png",
-  "/images/testimonials/Screenshot_20240704-234844[1] 1.png",
-  "/images/testimonials/Screenshot_20240705-134218[1] 1.png",
-  "/images/testimonials/Screenshot_20240705-133312[1] 1.png",
+    "/images/testimonials/testimonial-1.jpg",
+    "/images/testimonials/testimonial-2.jpg",
+    "/images/testimonials/testimonial-3.jpg",
+    "/images/testimonials/testimonial-1.jpg",
+    "/images/testimonials/testimonial-2.jpg",
+    "/images/testimonials/testimonial-3.jpg",
 ];
